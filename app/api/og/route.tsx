b591c65f67b74a16c1d7378ab0bf7738b2612458
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-08-11 19:31:20 +0800

    :sparkles: Complete MVP

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,31 +1,38 @@
-import { ImageResponse } from 'next/server';
+import {ImageResponse} from 'next/server';
 // App router includes @vercel/og.
 // No need to install it.
 
 export const runtime = 'edge';
 
-export async function GET() {
+export async function GET(req: Request) {
+
+    const url = new URL(req.url);
+    const searchParams = url.searchParams;
+    const name = searchParams.get('s') ?? 'X';
+    const hex = searchParams.get('hex') ?? '000000';
+    const size =  Number(searchParams.get('size')) || 40;
+    console.log(name);
+
     return new ImageResponse(
         (
             <div
                 style={{
                     fontSize: 40,
                     color: 'black',
-                    background: 'white',
+                    background: `#${hex}`,
                     width: '100%',
                     height: '100%',
-                    padding: '50px 200px',
                     textAlign: 'center',
                     justifyContent: 'center',
                     alignItems: 'center',
                 }}
             >
-                👋 Hello 你好 नमस्ते こんにちは สวัสดีค่ะ 안녕 добрий день Hallá
+                {name}
             </div>
         ),
         {
-            width: 1200,
-            height: 630,
+            width: size,
+            height: size,
         },
     );
 }
